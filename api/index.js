5fbca1b73218e932d5573d62b964d4bd328cf798
Eric Sakmar <eric.sakmar@gmail.com>
2015-10-20 19:41:40 -0400

    adds angular

--- api/index.js
+++ api/index.js
@@ -11,12 +11,35 @@ router.get('/forecast/:lat,:lon,:time', function(req, res, next) {
             + req.params.lon + ","
             + req.params.time;
 
+  /*
   http.get(url, function (fRes) {
     fRes.on('data', function (d) {
       var forecast = JSON.parse(d.toString('utf8'));
       res.json(forecast.currently);
     });
   });
+  */
+
+  var temp = {
+    "time":1445338800,
+    "summary":"Clear",
+    "icon":"clear-night",
+    "precipIntensity":0,
+    "precipProbability":0,
+    "temperature":42.31,
+    "apparentTemperature":40.31,
+    "dewPoint":35.8,
+    "humidity":0.78,
+    "windSpeed":3.66,
+    "windBearing":204,
+    "visibility":6.27,
+    "cloudCover":0.14,
+    "pressure":1027.7,
+    "ozone":294.65
+  };
+  res.json(temp);
+
+
 });
 
 module.exports = router;
