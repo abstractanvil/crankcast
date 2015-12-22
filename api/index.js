87be9ef41bedb579dc3d5190c075f4ff3e8a6d3f
Eric Sakmar <eric.sakmar@gmail.com>
2015-12-22 15:46:40 -0500

    updates styles

--- api/index.js
+++ api/index.js
@@ -21,25 +21,18 @@ router.get('/forecast/:lat,:lon,:time', function(req, res, next) {
   */
 
   var temp = {
-    "time":1445338800,
     "summary":"Clear",
-    "icon":"clear-night",
     "precipIntensity":0,
     "precipProbability":0,
     "temperature":42.31,
     "apparentTemperature":40.31,
-    "dewPoint":35.8,
     "humidity":0.78,
     "windSpeed":3.66,
     "windBearing":204,
-    "visibility":6.27,
-    "cloudCover":0.14,
-    "pressure":1027.7,
-    "ozone":294.65
+    "cloudCover":0.14
   };
+
   res.json(temp);
-
-
 });
 
 module.exports = router;
